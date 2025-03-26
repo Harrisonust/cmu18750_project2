9e1dd5ef8fd45416dfb5256d8b727993ad4adfea
Akshat Sahay <akshat412000@gmail.com>
2025-03-26 15:18:38 -0400

    Updated TX node for star network

--- StarNetwork/TX/code.py
+++ StarNetwork/TX/code.py
@@ -2,24 +2,31 @@
 # SPDX-License-Identifier: MIT
 
 """
-CircuitPython Feather RP2040 RFM95 Packet Send Demo
-
-This demo sends a "button" packet when the Boot button is pressed.
-
-This example is meant to be paired with the Packet Receive Demo code running
-on a second Feather RP2040 RFM95 board.
+18750 Project 2
+################
+Star Network: TX Node
 """
 
+import time
 import board
 import digitalio
-import keypad
+import random
+
 import adafruit_rfm9x
 
-# Set up button using keypad module.
-button = keypad.Keys((board.BUTTON,), value_when_pressed=False)
+"""
+color_values = [
+    (255, 0, 0),        # red
+    (0, 255, 0),        # green
+    (0, 0, 255),        # blue
+    (255, 255, 0),      # yellow
+    (0, 255, 255),      # cyan
+    (255, 0, 255),      # purple
+    (255, 255, 255),    # white
+]
+"""
 
-# Define radio frequency in MHz. Must match your
-# module. Can be a value like 915.0, 433.0, etc.
+# Define radio frequency in MHz
 RADIO_FREQ_MHZ = 915.0
 
 # Define Chip Select and Reset pins for the radio module.
@@ -28,25 +35,35 @@ RESET = digitalio.DigitalInOut(board.RFM_RST)
 
 # Initialise RFM95 radio
 rfm95 = adafruit_rfm9x.RFM9x(board.SPI(), CS, RESET, RADIO_FREQ_MHZ)
-rfm95.node = 77
-rfm95.destination = 7
-rfm95.ack_retries = 5
-rfm95.ack_wait = 0.5
 
-# Set LoRa TX power and parameters
-rfm95.high_power = False
-rfm95.tx_power = 5
+# Set node and LoRa parameters
+rfm95.node = 0x00
+rfm95.ack_retries = 0
 
 rfm95.signal_bandwidth = 500000
 rfm95.spreading_factor = 12
 rfm95.coding_rate = 8
 
+def send():
+    # Generate random destination
+    rfm95.destination = random.randint(1, 2)
+
+    # Generate random payload of colors
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    payload = bytes([r, g, b])
+
+    # Debug statement
+    print(f"Sending packet (src={rfm95.node}, dst={rfm95.destination}) color ({r}, {g}, {b})")
+
+    # Send the packet and see if we get an ACK back
+    if rfm95.send_with_ack(payload):
+        print("Received ACK")
+    else:
+        print(f"Failed to receive ACK")
+
+
 while True:
-    button_press = button.events.get()
-    if button_press:
-        if button_press.pressed:
-            print(f"sending package... (src={rfm95.node}, dst={rfm95.destination})")
-            if rfm95.send_with_ack(bytes("button", "UTF-8")):
-                print("received ack")
-            else:
-                print(f"failed to receive ack (retry: {rfm95.ack_retries}, ack_timeout: {rfm95.ack_wait} sec)")
+    send()
+    time.sleep(5)
