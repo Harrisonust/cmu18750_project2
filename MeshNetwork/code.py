eb0033a3636a63344504172de4d1229f44e23bed
Harrisonust <henhen.luo@gmail.com>
2025-03-28 15:41:16 -0400

    change board_id.py -> proj_config.py

--- MeshNetwork/code.py
+++ MeshNetwork/code.py
@@ -12,7 +12,7 @@ import random
 import digitalio
 import neopixel
 import adafruit_rfm9x
-import board_id
+import proj_config
 import time
 
 ### NEOPIXEL ###
@@ -40,10 +40,11 @@ RESET = digitalio.DigitalInOut(board.RFM_RST)
 rfm95 = adafruit_rfm9x.RFM9x(board.SPI(), CS, RESET, RADIO_FREQ_MHZ)
 
 # Set node
-if board_id.NODE_ID is None:
-    print("please set NODE_ID in board_id.py")
+if proj_config.NODE_ID is None:
+    print("please set NODE_ID in proj_config.py")
+    time.sleep(0xFFFFFFFF)
 nodes = [0x00, 0x01, 0x02, 0x03]
-nodes.remove(board_id.NODE_ID)
+nodes.remove(proj_config.NODE_ID)
 
 # Set LoRa parameters
 rfm95.signal_bandwidth = 500000
@@ -62,50 +63,50 @@ def send():
     global num_ack
     
     # Generate random destination
-    rfm95.node = board_id.NODE_ID
+    rfm95.node = proj_config.NODE_ID
     rfm95.destination = random.choice(nodes)
 
     # Generate random payload of colors
     color, color_name = random.choice(list(color_values.items()))
 
     # Debug statement
-    print(f"[TX {board_id.NODE_ID}] Sending packet from src={rfm95.node} to dst={rfm95.destination}, color={color}")
+    print(f"[TX {proj_config.NODE_ID}] Sending packet from src={rfm95.node} to dst={rfm95.destination}, color={color}")
 
     # Send the packet and see if we get an ACK back
     payload = bytes(color)
     num_send += 1
     if rfm95.send_with_ack(payload):
-        print(f"[TX {board_id.NODE_ID}] Received ACK")
+        print(f"[TX {proj_config.NODE_ID}] Received ACK")
         num_ack += 1
-        print(f"[TX {board_id.NODE_ID}] @@@ Sending color {color_name} @@@")
+        print(f"[TX {proj_config.NODE_ID}] @@@ Sending color {color_name} @@@")
     else:
-        print(f"[TX {board_id.NODE_ID}] Failed to receive ACK")
+        print(f"[TX {proj_config.NODE_ID}] Failed to receive ACK")
         
 
 def recv():
     global num_recv
     
     # Look for a new packet - wait up to 5 seconds:
-    print(f"[RX {board_id.NODE_ID}] Waiting for packets from other nodes")
+    print(f"[RX {proj_config.NODE_ID}] Waiting for packets from other nodes")
     packet = rfm95.receive(timeout=3, with_header=True, with_ack=True)
 
     # If no packet was received during the timeout then None is returned.
     if packet is not None:
         (dest, node, packet_id, flag), payload = packet[:4], packet[4:]
         if len(payload) != 3:
-            print(f"[RX {board_id.NODE_ID}] Payload corrupted {payload}")
+            print(f"[RX {proj_config.NODE_ID}] Payload corrupted {payload}")
 
         color = tuple(payload)        
         if color in color_values:
             color_name = color_values[color]
-            print(f"[RX {board_id.NODE_ID}] Received packet from src={node} to dst={dest}, color={color}, snr = {rfm95.last_snr}, rssi = {rfm95.last_rssi}")
+            print(f"[RX {proj_config.NODE_ID}] Received packet from src={node} to dst={dest}, color={color}, snr = {rfm95.last_snr}, rssi = {rfm95.last_rssi}")
 
             # fill led color
-            print(f"[RX {board_id.NODE_ID}] @@@ Changing color to {color_name} @@@")
+            print(f"[RX {proj_config.NODE_ID}] @@@ Changing color to {color_name} @@@")
             pixel.fill(color)
             num_recv += 1
         else: 
-            print(f"[RX {board_id.NODE_ID}] Payload corrupted {payload}")
+            print(f"[RX {proj_config.NODE_ID}] Payload corrupted {payload}")
 
 
 
