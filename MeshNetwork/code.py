fe8d6912579e6092d7b4e32fdd75ccdf4f04c7e6
Akshat Sahay <akshat412000@gmail.com>
2025-04-02 13:45:25 -0400

    RTS/CTS logic in main task

--- MeshNetwork/code.py
+++ MeshNetwork/code.py
@@ -17,6 +17,9 @@ import time
 from proj_config import NODE_ID
 from rts_cts_node import RTS_CTS_NODE
 
+def node_sleep():
+    pixel.fill(0, 0, 0)
+    time.sleep(2)
 
 ### NEOPIXEL ###
 pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
@@ -24,7 +27,6 @@ pixel.brightness = 0.5
 color_index = 0
 
 color_values = {
-    (255, 0, 0):    "red",
     (0, 255, 0):    "green",
     (0, 0, 255):    "blue",
     (255, 255, 0):  "yellow",
@@ -32,17 +34,50 @@ color_values = {
     (255, 0, 255):  "purple",
 }
 
+node = RTS_CTS_NODE()
+
 while True:
     # Based on choice, decide to TX or RX
     choice = random.randint(0, 100)
 
     if choice < 50:
-        # Node will transmit to a random destination
-        send(rfm95)
+        """ ---- Node is in TX mode ---- """
+
+        # Set pixel to red for indicating TX
+        pixel.fill(255, 0, 0)
+
+        # Send RTS to random dest and wait for CTS
+        node.send_rts()
+        flag_cts = node.wait_cts()
+
+        # Check if we got a CTS
+        if flag_cts == 0x00:
+            # Got a valid CTS from the dest!
+
+            # Generate random payload of colors
+            color, color_name = random.choice(list(color_values.items()))
+            payload = bytes(color) + b'\x55' * 247
+
+            # Send message to the dest and wait for an ack
+            node.send_msg(payload)
+
+        elif flag_cts == 0x01:
+            # CTS from another node, sleep
+            node_sleep()
+        
+        else:
+            # No response from dest to the RTS
+            continue
 
     else:
-        # Node will be ready to receive from other nodes
-        recv(rfm95)
+        """ ---- Node is in RX mode ---- """
+
+        # Wait for an RTS or CTS packet
+        flag_rts = node.wait_rts()
+
+        
+
+        pass
 
     success_rate = f"{num_ack / num_send * 100:.2f}%" if num_send != 0 else "NA"
     print(f"----- send:{num_send}/ack:{num_ack}/recv:{num_recv}/success:{success_rate} -----")
