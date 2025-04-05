d6739b0081161d2a88d335601facb4827fb213e0
Akshat Sahay <akshat412000@gmail.com>
2025-04-05 13:48:23 -0400

    Changes to RTS_CTS_NODE class

--- MeshNetwork/code.py
+++ MeshNetwork/code.py
@@ -15,9 +15,18 @@ import time
 from proj_config import NODE_ID
 from rts_cts_node import RTS_CTS_NODE, RTS_CTS_Error
 
-def node_sleep():
-    pixel.fill(0, 0, 0)
-    time.sleep(2)
+# Initialize RTS-CTS node
+node = RTS_CTS_NODE()
+
+# Initialize list of neighboring nodes
+neighbors = [0x00, 0x01, 0x02, 0x03]
+neighbors.remove(NODE_ID)
+
+# Node we are currently trying to transmit to
+request_node = 0xFF
+
+# Node currently trying to transmit to us
+receive_node = 0xFF
 
 ### NEOPIXEL ###
 pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
@@ -32,64 +41,72 @@ color_values = {
     (255, 0, 255):  "purple",
 }
 
-node = RTS_CTS_NODE()
 
-while True:
-    # Based on choice, decide to TX or RX
-    choice = random.randint(0, 100)
+# Function for a node sleeping when channel is busy
+def node_sleep():
+    pixel.fill(0, 0, 0)
+    time.sleep(2)
 
-    if choice < 50:
-        """ ---- Node is in TX mode ---- """
 
-        # Set pixel to red for indicating TX
-        pixel.fill(255, 0, 0)
+if __name__ == '__main__':
+        while True:
+        # Based on choice, decide to TX or RX
+        choice = random.randint(0, 100)
 
-        # Send RTS to random dest and wait for CTS
-        node.send_rts()
-        flag_cts = node.wait_cts()
+        if choice < 50:
+            """ ---- Node is in TX mode ---- """
 
-        # Check return val for wait_cts()
-        if flag_cts == RTS_CTS_Error.SUCCESS:
-            # Got a valid CTS from the dest!
+            # Set pixel to red for indicating TX
+            pixel.fill(255, 0, 0)
 
-            # Generate random payload of colors
-            color, color_name = random.choice(list(color_values.items()))
-            payload = bytes(color) + b'\x55' * 247
+            # Send RTS to random dest and wait for CTS
+            request_node = random.choice(neighbors)
 
-            # Send message to the dest and wait for an ack
-            node.send_msg(payload)
+            node.send_rts(request_node)
+            flag_cts = node.wait_cts()
 
-        elif flag_cts == RTS_CTS_Error.CTS_WRONG:
-            # Got a CTS from another node, so channel is busy
-            node_sleep()
-        
-        else:
-            # No response from dest to the RTS
-            pass
+            # Check return val for wait_cts()
+            if flag_cts == RTS_CTS_Error.SUCCESS:
+                # Got a valid CTS from the dest!
 
-    else:
-        """ ---- Node is in RX mode ---- """
+                # Generate random payload of colors
+                color, color_name = random.choice(list(color_values.items()))
+                payload = bytes(color) + b'\x55' * 247
 
-        # Wait for an RTS or CTS packet
-        flag_rts = node.wait_rts()
+                # Send message to the dest and wait for an ack
+                node.send_msg(payload)
 
-        # Check return val for wait_rts()
-        if flag_rts == RTS_CTS_Error.SUCCESS:
-            # Got a valid RTS from a node, meant for this node!
-
-            # Send a CTS as a broadcast to all nodes, indicating channel busy
-            node.send_cts()
-
-            # Wait for an actual message from the RTS node
-            # Message will have an inbuilt ACK
-            node.recv_msg()
-
-        elif flag_rts == RTS_CTS_Error.RTS_WRONG:
-            # Got a CTS from another node, so channel is busy
-            node_sleep()
+            elif flag_cts == RTS_CTS_Error.CTS_WRONG:
+                # Got a CTS from another node, so channel is busy
+                node_sleep()
+            
+            else:
+                # No response from dest to the RTS
+                pass
 
         else:
-            # No CTS received
-            pass
+            """ ---- Node is in RX mode ---- """
 
-    print(node.get_stats())
+            # Wait for an RTS or CTS packet
+            flag_rts = node.wait_rts()
+
+            # Check return val for wait_rts()
+            if flag_rts == RTS_CTS_Error.SUCCESS:
+                # Got a valid RTS from a node, meant for this node!
+
+                # Send a CTS as a broadcast to all nodes, indicating channel busy
+                node.send_cts()
+
+                # Wait for an actual message from the RTS node
+                # Message will have an inbuilt ACK
+                node.recv_msg()
+
+            elif flag_rts == RTS_CTS_Error.RTS_WRONG:
+                # Got a CTS from another node, so channel is busy
+                node_sleep()
+
+            else:
+                # No CTS received
+                pass
+
+        print(node.get_stats())
--- MeshNetwork/rts_cts_node.py
+++ MeshNetwork/rts_cts_node.py
@@ -6,14 +6,19 @@ from proj_config import NODE_ID
 import adafruit_logging as logging
 
 class RTS_CTS_Error():
-    SUCCESS         = 0
-    PACKAGE_CORRUPT = 1
-    RTS_WRONG       = 2
-    RTS_TIMEOUT     = 3
-    CTS_WRONG       = 4
-    CTS_TIMEOUT     = 5
-    ACK_WRONG       = 6
-    ACK_TIMEOUT     = 7
+    SUCCESS         = 0  # Success in RTS or CTS
+    PACKAGE_CORRUPT = 1  # Packet corrupted in transmission
+
+    RTS_WRONG       = 2  # Incorrect RTS format
+    RTS_TIMEOUT     = 3  # No RTS received
+
+    CTS_WRONG       = 4 # Incorrect CTS format
+    CTS_NOT_DEST    = 5  # CTS not from RTS destination
+    CTS_TIMEOUT     = 6  # No CTS received
+
+    ACK_WRONG       = 7  # Incorrect ACK format
+    ACK_TIMEOUT     = 8  # No ACK received
+
 
 class RTS_CTS_NODE(RFM9x):
     # A single RTS/CTS node for the mesh network
@@ -56,107 +61,189 @@ class RTS_CTS_NODE(RFM9x):
         self.num_recv = 0
         self.num_ack  = 0
 
+        # Last node that transmitted to us
+        self.last_node = 0xFF
+
     def send_raw(self, dest, control:bytes=None, payload:bytes=None) -> None:
+        # Send any data and log to the logger
         data = b""
+
+        # Add control byte for message
         if control is not None:
             data += control
+        else:
+            raise Exception("[CRITICAL ERROR] Tried transmitting without a control byte")
+
+        # Add payload for message, some messages may not need one
         if payload is not None:
             data += payload
 
-        self.logger.info(f"[{self.node_id}] Sending packet from src={self.node_id} to dst={dest}, payload={payload}")
+        # Log info and send
+        self.logger.info(f"[{self.node_id}] Sending packet from src={self.node_id} to dst={dest}")
         self.send(data=data, node=self.node_id, destination=dest)
 
     def recv_raw(self) -> bytes:
-        # self.logger.info(f"[RX {self.node_id}] Waiting for packets from other nodes")
+        # Receive any data and log to the logger
+        self.logger.info(f"[{self.node_id}] Waiting for packets from other nodes")
         packet = self.receive(timeout=1, with_header=True)
 
         if packet is None:
-            return None 
+            # No packet received
+            return None
+
         if len(packet) > 4:
+            # Extract RadioHead header params
             (dest, node, packet_id, flag), payload = packet[:4], packet[4:]
 
-            if len(payload) != 250:
-                print(f"[RX {self.NODE_ID}] Payload corrupted {len(payload)=}")
-                return
+            # Save the node from this transmission as the last_node
+            self.last_node = node
 
-            print(f"[RX {self.NODE_ID}] Received packet from src={node} to dst={dest}, snr = {self.rfm95.last_snr}, rssi = {self.rfm95.last_rssi}")
-            self.num_recv += 1
-    
+            # Increase num_recv
+            self.logger.info(f"[{self.NODE_ID}] Received packet from src={node} to dst={dest}, snr = {self.rfm95.last_snr}, rssi = {self.rfm95.last_rssi}")
+
+            return payload
+
+        else:
+            return None
+
     def send_msg(self, rx_node, payload) -> None:
+        # Send a 250 byte message to rx_node
+        self.logger.info(f"[TX {self.NODE_ID}] Sending message to {rx_node}")
         self.send_raw(dest=rx_node, control=self.CONTROL_MSG, payload=payload)
         self.num_send += 1
-    
+
     def recv_msg(self) -> bytes:
+        # Receive 250 byte message from self.last_node
         payload = self.recv_raw()
+
+        # Check for a valid payload
         if payload is None:
             return None
-        
+
+        # Check if the message length is correct
+        if len(payload) != 250:
+            self.logger.warning(f"[RX {self.NODE_ID}] Received a corrupted payload (wrong len)!")
+            return None
+
+        # Check if the control byte for the message is correct
+        if payload[1] != self.CONTROL_MSG:
+            self.logger.warning(f"[RX {self.NODE_ID}] Received a corrupted payload (wrong control)!")
+            return None
+
+        # Message passed all checks, return payload except control byte
         self.num_recv += 1
         return payload[1:]
 
-    def send_cts(self, approved_node: bytes):
-        self.send_raw(dest=self.BROADCAST_ADDRESS, control=self.CONTROL_CTS+approved_node.to_bytes(1))
-
-    def wait_cts(self) -> RTS_CTS_Error:
-        ret = self.recv_raw()
-        if ret is None:
-            self.logger.debug(f"[{self.node_id}] cts timeout")
-            return RTS_CTS_Error.CTS_TIMEOUT
-        elif len(ret) != 2:
-            self.logger.debug(f"[{self.node_id}] wrong cts (len(ret) != 2)")
-            return RTS_CTS_Error.CTS_WRONG
-            
-        control1 = ret[0].to_bytes(1)  
-        control2 = ret[1] # address is in int
-        if control1 == self.CONTROL_CTS and control2 == self.node_id:
-            self.logger.debug(f"[{self.node_id}] got cts")
-            return RTS_CTS_Error.SUCCESS 
-        else:
-            if control1 != self.CONTROL_CTS:
-                self.logger.debug(f"[{self.node_id}] wrong cts (control1 != self.CONTROL_CTS)")
-            if control2 != self.node_id:
-                self.logger.debug(f"[{self.node_id}] wrong cts (control2 != self.node_id)")
-            return RTS_CTS_Error.CTS_WRONG
- 
     def send_rts(self, request_node) -> None:
+        # Send an RTS packet: control byte only
+        self.logger.info(f"[TX {self.NODE_ID}] Sending RTS to {request_node}")
         self.send_raw(dest=request_node, control=self.CONTROL_RTS)
 
     def wait_rts(self) -> RTS_CTS_Error:
+        self.logger.info(f"[RX {self.NODE_ID}] Waiting for a valid RTS")
+
+        # Receive the return value from recv_raw
         ret = self.recv_raw()
+
+        # Check for RTS timeout
         if ret is None:
-            self.logger.debug(f"[{self.node_id}] rts timeout")
+            self.logger.warning(f"[RX {self.node_id}] RTS Timeout")
             return RTS_CTS_Error.RTS_TIMEOUT
+
+        # Check for RTS format
         elif len(ret) != 1:
-            self.logger.debug(f"[{self.node_id}] wrong rts (len(ret) != 1)")
+            self.logger.warning(f"[RX {self.node_id}] Wrong RTS format (wrong len)")
             return RTS_CTS_Error.RTS_WRONG
         
         control = ret
+
+        # Check for RTS control byte
         if control == self.CONTROL_RTS:
-            self.logger.debug(f"[{self.node_id}] got rts")
-            return RTS_CTS_Error.SUCCESS 
+            self.logger.info(f"[RX {self.node_id}] Got a valid RTS from {self.last_node}")
+            return RTS_CTS_Error.SUCCESS
+
         else:
-            self.logger.debug(f"[{self.node_id}] wrong rts (control != self.CONTROL_RTS)")
+            self.logger.warning(f"[RX {self.node_id}] Wrong RTS format (wrong control byte)")
             return RTS_CTS_Error.RTS_WRONG
 
-    def send_ack(self, tx_node) -> None:
-        self.send_raw(dest=tx_node, control=self.CONTROL_ACK)
-    
-    def wait_ack(self) -> RTS_CTS_Error:
+    def send_cts(self, approved_node: bytes):
+        # Send a broadcast CTS, specifying which node is clear to send
+        self.logger.info(f"[RX {self.NODE_ID}] Sending CTS to {approved_node}")
+        self.send_raw(dest=self.BROADCAST_ADDRESS, control=self.CONTROL_CTS+approved_node.to_bytes(1))
+
+    def wait_cts(self, request_node) -> RTS_CTS_Error:
+        # Receive a valid CTS from the node we sent an RTS to
+        self.logger.info(f"[TX {self.NODE_ID}] Waiting for valid CTS from {request_node}")
         ret = self.recv_raw()
+
+        # Check for CTS timeout
         if ret is None:
-            self.logger.debug(f"[{self.node_id}] act timeout")
+            self.logger.warning(f"[TX {self.node_id}] CTS timeout")
+            return RTS_CTS_Error.CTS_TIMEOUT
+
+        elif len(ret) != 2:
+            self.logger.warning(f"[TX {self.node_id}] Wrong CTS format (wrong len)")
+            return RTS_CTS_Error.CTS_WRONG
+
+        # In ret, control1 is control byte, control2 is node that is clear to send
+        control1 = ret[0].to_bytes(1)
+
+        # No bytes conversion, ret is an int list
+        control2 = ret[1]
+
+        # Check for CTS control byte and specified node in CTS
+        if control1 == self.CONTROL_CTS and control2 == self.node_id:
+            # Check if the node that send the CTS is request_node
+            if self.last_node == request_node:
+                # Got a valid CTS
+                self.logger.info(f"[TX {self.node_id}] Got a valid CTS from {request_node}")
+                return RTS_CTS_Error.SUCCESS
+
+            else:
+                # Weird error, a node we never tried talking to cleared us to send
+                self.logger.error(f"[TX {self.node_id}] CTS not received from {request_node}")
+                raise Exception("CTS from a node we never sent RTS to")
+
+        else:
+            # Check if the message even is a CTS message
+            if control1 != self.CONTROL_CTS:
+                self.logger.warning(f"[TX {self.node_id}] Not a CTS message")
+                return RTS_CTS_Error.CTS_WRONG
+
+            # Check if the CTS was meant for us
+            if control2 != self.node_id:
+                self.logger.warning(f"[TX {self.node_id}] CTS meant for a different node")
+                return RTS_CTS_Error.CTS_NOT_DEST
+
+    def send_ack(self, tx_node) -> None:
+        # Send an ACK in response to a message
+        self.logger.info(f"[RX {self.NODE_ID}] Sending ACK to {tx_node}")
+        self.send_raw(dest=tx_node, control=self.CONTROL_ACK)
+
+    def wait_ack(self) -> RTS_CTS_Error:
+        # After transmitting a message, wait for an ACK
+        ret = self.recv_raw()
+
+        # Check for ACK timeout
+        if ret is None:
+            self.logger.warning(f"TX [{self.node_id}] ACK timeout")
             return RTS_CTS_Error.ACK_TIMEOUT
+
+        # Check for ACK format
         elif len(ret) != 1:
-            self.logger.debug(f"[{self.node_id}] wrong ack (len(ret) != 1)")
+            self.logger.warning(f"[{self.node_id}] Wrong ACK format (wrong len)")
             return RTS_CTS_Error.ACK_WRONG
         
         control = ret
+
+        # Check control byte for message
         if control == self.CONTROL_ACK:
-            self.logger.debug(f"[{self.node_id}] got ack")
+            self.logger.info(f"[{self.node_id}] Got an ACK from {self.last_node}")
             self.num_ack += 1
             return RTS_CTS_Error.SUCCESS
+
         else:
-            self.logger.debug(f"[{self.node_id}] wrong ack (control != self.CONTROL_ACK)")
+            self.logger.warning(f"[{self.node_id}] Not an ACK")
             return RTS_CTS_Error.ACK_WRONG
 
     def get_stats(self):
