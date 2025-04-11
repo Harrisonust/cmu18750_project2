5f3e488a90531d28ee8de47e2023e493def5511d
Harrisonust <henhen.luo@gmail.com>
2025-04-11 13:19:09 -0400

    refactor lib location and create fdma template

---
+++ FDMA/code.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: 2023 Kattni Rembor for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+"""
+18750 Project 2
+################
+Mesh Network
+"""
+
+import board
+import random
+import neopixel
+import time
+
+from proj_config import NODE_ID
+
+if __name__ == '__main__':
+    while True:
+        pass
+
+if __name__ == '__main__':
+    main()
