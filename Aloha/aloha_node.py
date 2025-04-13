28cfcaebea8beb467c39679cc4891a87116dae85
Harrisonust <henhen.luo@gmail.com>
2025-04-13 14:56:49 -0400

    refactor aloha

---
+++ Aloha/aloha_node.py
@@ -0,0 +1,80 @@
+import time
+import board
+import digitalio
+from adafruit_rfm9x import RFM9x
+import adafruit_logging as logging
+from proj_config import NODE_ID
+
+class Aloha_Node(RFM9x):
+    def __init__(self):
+        self.logger = logging.getLogger('RTS_CTS')
+        self.logger.setLevel(logging.DEBUG)
+        
+        # Define Chip Select and Reset pins for the radio module.
+        cs = digitalio.DigitalInOut(board.RFM_CS)
+        reset = digitalio.DigitalInOut(board.RFM_RST)
+        radio_freq_mhz = 915.0
+
+        # Initialise RFM95 radio
+        RFM9x.__init__(self, board.SPI(), cs, reset, radio_freq_mhz)
+
+        # Set node
+        if NODE_ID is None:
+            self.logger.error("Please set NODE_ID in proj_config.py")
+            time.sleep(0xFFFFFFFF)
+
+        # self.node is internal to the driver, but also used for our logs
+        self.node = NODE_ID
+        self.BROADCAST_ADDRESS = 255
+
+        # Set LoRa parameters
+        self.signal_bandwidth = 500000
+        self.spreading_factor = 7
+        self.coding_rate = 8
+        self.ack_retries = 0
+        self.ack_wait = 1
+
+        # Packet length definitions
+        self.MAX_PAYLOAD_LEN = 250
+
+        # Counter variables
+        self.num_send = 0
+        self.num_recv = 0
+        self.num_ack  = 0
+        self.node_start_time = time.monotonic()
+        self.sent_bytes = 0
+
+    def send_msg(self, rx_node, payload) -> None:
+        # Debug statement
+        self.logger.info(f"[TX {self.node}] Sending packet from src={self.node} to dst={rx_node}")
+        self.destination = rx_node
+        # Send the packet and see if we get an ACK back
+        self.num_send += 1
+        if self.send_with_ack(payload):
+            self.logger.info(f"[TX {self.node}] Received ACK")
+            self.sent_bytes += self.MAX_PAYLOAD_LEN
+            self.num_ack += 1
+        else:
+            self.logger.info(f"[TX {self.node}] Failed to receive ACK")
+
+    def recv_msg(self) -> bytes:
+        # Look for a new packet - wait up to 5 seconds:
+        self.logger.info(f"[RX {self.node}] Waiting for packets from other nodes")
+        packet = self.receive(timeout=3, with_header=True, with_ack=True)
+
+        # If no packet was received during the timeout then None is returned.
+        if packet is not None:
+            (dest, node, packet_id, flag), payload = packet[:4], packet[4:]
+            if len(payload) != self.MAX_PAYLOAD_LEN:
+                self.logger.info(f"[RX {self.node}] Payload corrupted {payload}")
+                return None
+            else:
+                self.num_recv += 1
+                return payload
+        return None
+    
+    def get_stats(self):
+        time_elapsed = time.monotonic() - self.node_start_time
+        throughput = self.sent_bytes * 8 / time_elapsed # in bps
+        success_rate = f"{self.num_ack / self.num_send * 100:.2f}%" if self.num_send else "NA"
+        return f"----- send:{self.num_send}/ack:{self.num_ack}/recv:{self.num_recv}/success:{success_rate}/throughput:{throughput:.2f}bps -----"
--- Aloha/code.py
+++ Aloha/code.py
@@ -4,23 +4,30 @@
 """
 18750 Project 2
 ################
-Mesh Network
+Aloha Network
 """
 
 import board
 import random
-import digitalio
 import neopixel
-import adafruit_rfm9x
 import time
+
 from proj_config import NODE_ID
+from aloha_node import Aloha_Node
+
+# Initialize Aloha node
+node = Aloha_Node()
+
+# Initialize list of neighboring nodes
+neighbors = [0x00, 0x01, 0x02, 0x03]
+neighbors.remove(NODE_ID)
 
 ### NEOPIXEL ###
 pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
 pixel.brightness = 0.5
 color_index = 0
 
-color_values = {
+color_map = {
     (255, 0, 0):    "red",
     (0, 255, 0):    "green",
     (0, 0, 255):    "blue",
@@ -29,101 +36,27 @@ color_values = {
     (255, 0, 255):  "purple",
 }
 
-# Define Chip Select and Reset pins for the radio module.
-CS = digitalio.DigitalInOut(board.RFM_CS)
-RESET = digitalio.DigitalInOut(board.RFM_RST)
-
-# Initialise RFM95 radio
-RADIO_FREQ_MHZ = 915.0
-rfm95 = adafruit_rfm9x.RFM9x(board.SPI(), CS, RESET, RADIO_FREQ_MHZ)
-
-# Set node
-if NODE_ID is None:
-    print("please set NODE_ID in proj_config.py")
-    time.sleep(0xFFFFFFFF)
-nodes = [0x00, 0x01, 0x02, 0x03]
-nodes.remove(NODE_ID)
-
-# Set LoRa parameters
-rfm95.signal_bandwidth = 500000
-rfm95.spreading_factor = 7
-rfm95.coding_rate = 8
-rfm95.ack_retries = 0
-rfm95.ack_wait = 3
-
-# Counter variables
-num_send = 0
-num_recv = 0
-num_ack  = 0
-MAX_PAYLOAD_LEN = 250
-sent_bytes = 0
-def send():
-    global num_send
-    global num_ack
-    global sent_bytes
-
-    # Generate random destination
-    rfm95.node = NODE_ID
-    rfm95.destination = random.choice(nodes)
-
-    # Generate random payload of colors
-    color, color_name = random.choice(list(color_values.items()))
-
-    # Debug statement
-    print(f"[TX {NODE_ID}] Sending packet from src={rfm95.node} to dst={rfm95.destination}, color={color}")
-
-    # Send the packet and see if we get an ACK back
-    payload = bytes(color) + (MAX_PAYLOAD_LEN-len(color)) * b'\x55'
-    num_send += 1
-    if rfm95.send_with_ack(payload):
-        print(f"[TX {NODE_ID}] Received ACK")
-        num_ack += 1
-        sent_bytes += MAX_PAYLOAD_LEN
-        print(f"[TX {NODE_ID}] @@@ Sending color {color_name} @@@")
-    else:
-        print(f"[TX {NODE_ID}] Failed to receive ACK")
-        
-
-def recv():
-    global num_recv
-    
-    # Look for a new packet - wait up to 5 seconds:
-    print(f"[RX {NODE_ID}] Waiting for packets from other nodes")
-    packet = rfm95.receive(timeout=3, with_header=True, with_ack=True)
-
-    # If no packet was received during the timeout then None is returned.
-    if packet is not None:
-        (dest, node, packet_id, flag), payload = packet[:4], packet[4:]
-        if len(payload) != MAX_PAYLOAD_LEN:
-            print(f"[RX {NODE_ID}] Payload corrupted {payload}")
-
-        color = tuple(payload[:3])        
-        if color in color_values:
-            color_name = color_values[color]
-            print(f"[RX {NODE_ID}] Received packet from src={node} to dst={dest}, color={color}, snr = {rfm95.last_snr}, rssi = {rfm95.last_rssi}")
-
-            # fill led color
-            print(f"[RX {NODE_ID}] @@@ Changing color to {color_name} @@@")
-            pixel.fill(color)
-            num_recv += 1
-        else: 
-            print(f"[RX {NODE_ID}] Payload corrupted {payload}")
-
-if __name__ == "__main__":
-    node_start_time = time.monotonic()
+def main():
     while True:
         # Based on choice, decide to TX or RX
         choice = random.randint(0, 100)
 
         if choice < 50:
             # Node will transmit to a random destination
-            send()
+            rx_node = random.choice(neighbors)
+            color, color_name = random.choice(list(color_map.items()))
+            payload = bytes(color) + b'\x55' * (node.MAX_PAYLOAD_LEN - len(color))
+            node.send_msg(rx_node, payload)
 
         else:
             # Node will be ready to receive from other nodes
-            recv()
+            payload = node.recv_msg()
+            if payload is not None:
+                color = payload[:3]
+                pixel.fill(tuple(color))
 
-        time_elapsed = time.monotonic() - node_start_time
-        throughput = sent_bytes * 8 / time_elapsed # in bps
-        success_rate = f"{num_ack / num_send * 100:.2f}%" if num_send else "NA"
-        print(f"----- send:{num_send}/ack:{num_ack}/recv:{num_recv}/success:{success_rate}/throughput:{throughput:.2f}bps -----")
+        print(node.get_stats())
+        time.sleep(0.5)
+        
+if __name__ == '__main__':
+    main()
