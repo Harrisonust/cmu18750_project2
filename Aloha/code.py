506d7ad47d7d27f4c1c72878336c355d6815172e
Harrisonust <henhen.luo@gmail.com>
2025-04-13 14:13:48 -0400

    add throughput monitor and change payload len to 250

--- Aloha/code.py
+++ Aloha/code.py
@@ -55,11 +55,13 @@ rfm95.ack_wait = 3
 num_send = 0
 num_recv = 0
 num_ack  = 0
-
+MAX_PAYLOAD_LEN = 250
+sent_bytes = 0
 def send():
     global num_send
     global num_ack
-    
+    global sent_bytes
+
     # Generate random destination
     rfm95.node = NODE_ID
     rfm95.destination = random.choice(nodes)
@@ -71,11 +73,12 @@ def send():
     print(f"[TX {NODE_ID}] Sending packet from src={rfm95.node} to dst={rfm95.destination}, color={color}")
 
     # Send the packet and see if we get an ACK back
-    payload = bytes(color)
+    payload = bytes(color) + (MAX_PAYLOAD_LEN-len(color)) * b'\x55'
     num_send += 1
     if rfm95.send_with_ack(payload):
         print(f"[TX {NODE_ID}] Received ACK")
         num_ack += 1
+        sent_bytes += MAX_PAYLOAD_LEN
         print(f"[TX {NODE_ID}] @@@ Sending color {color_name} @@@")
     else:
         print(f"[TX {NODE_ID}] Failed to receive ACK")
@@ -91,10 +94,10 @@ def recv():
     # If no packet was received during the timeout then None is returned.
     if packet is not None:
         (dest, node, packet_id, flag), payload = packet[:4], packet[4:]
-        if len(payload) != 3:
+        if len(payload) != MAX_PAYLOAD_LEN:
             print(f"[RX {NODE_ID}] Payload corrupted {payload}")
 
-        color = tuple(payload)        
+        color = tuple(payload[:3])        
         if color in color_values:
             color_name = color_values[color]
             print(f"[RX {NODE_ID}] Received packet from src={node} to dst={dest}, color={color}, snr = {rfm95.last_snr}, rssi = {rfm95.last_rssi}")
@@ -106,18 +109,21 @@ def recv():
         else: 
             print(f"[RX {NODE_ID}] Payload corrupted {payload}")
 
+if __name__ == "__main__":
+    node_start_time = time.monotonic()
+    while True:
+        # Based on choice, decide to TX or RX
+        choice = random.randint(0, 100)
 
+        if choice < 50:
+            # Node will transmit to a random destination
+            send()
 
-while True:
-    # Based on choice, decide to TX or RX
-    choice = random.randint(0, 100)
+        else:
+            # Node will be ready to receive from other nodes
+            recv()
 
-    if choice < 50:
-        # Node will transmit to a random destination
-        send()
-
-    else:
-        # Node will be ready to receive from other nodes
-        recv()
-
-    print(f"-----send:{num_send}/ack:{num_ack}/recv:{num_recv} -----")
+        time_elapsed = time.monotonic() - node_start_time
+        throughput = sent_bytes * 8 / time_elapsed # in bps
+        success_rate = f"{num_ack / num_send * 100:.2f}%" if num_send else "NA"
+        print(f"----- send:{num_send}/ack:{num_ack}/recv:{num_recv}/success:{success_rate}/throughput:{throughput:.2f}bps -----")
